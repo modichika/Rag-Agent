b6b08e84ef2d1cf00e4194817d799216da788861
Shristi modi <shristimodi95@gmail.com>
2026-02-24 12:00:13 +0530

    split documents into smaller chunks

--- docs_loader.py
+++ docs_loader.py
@@ -14,6 +14,27 @@ docs = loader.load() # triggers actual netwrok request, return Document objects,
 
 assert len(docs) == 1 # ensures only one document is loaded
 print(f"Total characters: {len(docs[0].page_content)}")
-print(docs[0].page_content[:500]) # string slicing operation
-print(type(docs))
+#print(docs[0].page_content[:500]) # string slicing operation
+#print(type(docs))
+
+# For Splitting the document into smaller chunks.
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter( # the recursive one cuts the whole blog into smaller after paras, new lines, then individual chars. Not through any char("A-gent"). text_splitter is an object not a list, tuple whose length cannot be taken out 
+    chunk_size=1000, # size of each chunk in chars.
+    chunk_overlap=200, # from the end of chunk 1 it will appear in the beginning of chunk 2.
+    add_start_index=True, # a boolean flag to calculate the exact char position where each chunk begins i.e, the start char, it adds the start_index to the metadata dictionary of each chunk. A kind of bookmark to tell which chunk we are on
+)
+
+all_splits = text_splitter.split_documents(docs) # new list of smaller document object from the docs by looping through the docs.
+
+print(f"Split blog post into {len(all_splits)} sub-documents.") # we have 63 sub docs chunked each has 1000 chars.
+#print(text_splitter._chunk_size, text_splitter._chunk_overlap, text_splitter._add_start_index)
+#print(all_splits[0].metadata) # prints 2 everytime coz langchain attatches two specific labels to every single chunk : 1. source, 2. start_index.
+
+#print(repr(docs[0].page_content[:8]))
+
+# Note if the print of len of all_splits is 1 then our logic has failed.
+
+
 
